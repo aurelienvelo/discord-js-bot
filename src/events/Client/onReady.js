1b1035d388589c72492c475044b288518022553b
aurelienvelo <aurelien.velo@gmail.com>
2025-12-30 22:22:24 +0100

    event change clientReady (ready deprecated)

--- src/events/Client/onReady.js
+++ src/events/Client/onReady.js
@@ -2,7 +2,7 @@ const Event = require("../../structure/Event");
 const { setDiscordLogging } = require("../../utils/logger");
 
 module.exports = new Event({
-    event: 'ready',
+    event: 'clientReady',
     once: true,
     run: (__client__, client) => {
         __client__.logger.info('Logged in as ' + client.user.displayName + ', took ' + ((Date.now() - __client__.login_timestamp) / 1000) + "s.")
